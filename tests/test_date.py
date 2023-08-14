8401b2185d0baa2bef00b74e78d942c22a91a014
Juan Francisco Abellán García <121306222+Juan-Abellan@users.noreply.github.com>
2023-08-14 16:07:40 +0200

    Delete tests directory

--- tests/test_date.py
+++
@@ -1,6 +0,0 @@
-from nbresult import ChallengeResultTestCase
-
-
-class TestDate(ChallengeResultTestCase):
-    def test_month_column_is_a_datetime(self):
-        self.assertEqual(self.result.month_type, 'datetime64[ns]')
